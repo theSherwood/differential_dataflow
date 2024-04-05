9b579e6e5c41d2d36c13d759baa74e3e8733369a
Adam Sherwood <adam.the.sherwood@gmail.com>
2024-04-05 08:32:20 +0100

    mset -> collection

--- src/dida_from_python.nim
+++ src/dida_from_python.nim
@@ -2,6 +2,9 @@ import std/[tables, sets, bitops, strutils, sequtils, sugar]
 import hashes
 import values
 
+# Collection #
+# ---------------------------------------------------------------------
+
 type
   Key* = ImValue
   Value* = ImValue
@@ -9,15 +12,15 @@ type
 
   Row* = (Entry, int)
   
-  Multiset* = object
+  Collection* = object
     rows*: seq[Row]
 
   MapFn* = proc (e: Entry): Entry {.closure.}
   FilterFn* = proc (e: Entry): bool {.closure.}
   ReduceFn* = proc (rows: seq[Row]): seq[Row] {.closure.}
-  IterateFn* = proc (c: Multiset): Multiset {.closure.}
+  IterateFn* = proc (c: Collection): Collection {.closure.}
 
-proc size*(c: Multiset): int = return c.rows.len
+proc size*(c: Collection): int = return c.rows.len
 
 template key*(e: Entry): Key = e[0]
 template value*(e: Entry): Value = e[1]
@@ -26,18 +29,18 @@ template key*(r: Row): Key = r[0][0]
 template value*(r: Row): Value = r[0][1]
 template multiplicity*(r: Row): int = r[1]
 
-template `[]`*(c: Multiset, i: int): untyped = c.rows[i]
-template `[]=`*(c: Multiset, i: int, r: Row) = c.rows[i] = r
-template add*(c: Multiset, r: Row) = c.rows.add(r)
+template `[]`*(c: Collection, i: int): untyped = c.rows[i]
+template `[]=`*(c: Collection, i: int, r: Row) = c.rows[i] = r
+template add*(c: Collection, r: Row) = c.rows.add(r)
 
-iterator items*(c: Multiset): Row =
+iterator items*(c: Collection): Row =
   for r in c.rows:
     yield r
 
 ## This is quite an expensive operation. It would be good to find a faster way
 ## to compute this.
 ## Using an xor-based hash for entries could help a lot.
-proc `==`*(c1, c2: Multiset): bool =
+proc `==`*(c1, c2: Collection): bool =
   var
     t1 = initTable[Entry, int]()
     t2 = initTable[Entry, int]()
@@ -47,43 +50,43 @@ proc `==`*(c1, c2: Multiset): bool =
     t2[e] = m + t2.getOrDefault(e, 0)
   return t1 == t2
 
-proc map*(c: Multiset, f: MapFn): Multiset =
+proc map*(c: Collection, f: MapFn): Collection =
   result.rows.setLen(c.size)
   for i in 0..<c.size:
     result[i] = (f(c[i].entry), c[i].multiplicity)
 
-proc filter*(c: Multiset, f: FilterFn): Multiset =
+proc filter*(c: Collection, f: FilterFn): Collection =
   for r in c:
     if f(r.entry): result.add(r)
 
-proc negate*(c: Multiset): Multiset =
+proc negate*(c: Collection): Collection =
   result.rows.setLen(c.size)
   for i in 0..<c.size:
     result[i] = (c[i].entry, 0 - c[i].multiplicity)
 
-proc concat*(c1, c2: Multiset): Multiset =
+proc concat*(c1, c2: Collection): Collection =
   for r in c1: result.add(r)
   for r in c2: result.add(r)
 
-proc consolidate*(c: Multiset): Multiset =
+proc consolidate*(c: Collection): Collection =
   var t = initTable[Entry, int]()
   for (e, m) in c:
     t[e] = m + t.getOrDefault(e, 0)
   for e, m in t.pairs:
     if m != 0: result.add((e, m))
 
-proc print*(c: Multiset, label: string): Multiset =
+proc print*(c: Collection, label: string): Collection =
   echo label, ": ", c
   return c
 
-proc to_row_table_by_key(t: var Table[Key, seq[Row]], c: Multiset) =
+proc to_row_table_by_key(t: var Table[Key, seq[Row]], c: Collection) =
   for r in c:
     if t.hasKey(r.key):
       t[r.key].add(r)
     else:
       t[r.key] = @[r]
 
-proc join*(c1, c2: Multiset): Multiset =
+proc join*(c1, c2: Collection): Collection =
   let empty_seq = newSeq[Row]()
   var t = initTable[Key, seq[Row]]()
   t.to_row_table_by_key(c1)
@@ -92,7 +95,7 @@ proc join*(c1, c2: Multiset): Multiset =
       result.add(((r.key, init_array([r2.value, r.value]).v), r.multiplicity * r2.multiplicity))
 
 ## Keys must not be changed by the reduce fn
-proc reduce*(c: Multiset, f: ReduceFn): Multiset =
+proc reduce*(c: Collection, f: ReduceFn): Collection =
   var t = initTable[Key, seq[Row]]()
   t.to_row_table_by_key(c)
   for r in t.values:
@@ -105,7 +108,7 @@ proc count_inner(rows: seq[Row]): seq[Row] =
   for r in rows: cnt += r.multiplicity
   return @[((k, cnt.float64.v), 1)]
 
-proc count*(c: Multiset): Multiset =
+proc count*(c: Collection): Collection =
   return c.reduce(count_inner)
 
 proc sum_inner(rows: seq[Row]): seq[Row] =
@@ -114,7 +117,7 @@ proc sum_inner(rows: seq[Row]): seq[Row] =
   for r in rows: cnt += r.value.as_f64 * r.multiplicity.float64
   return @[((k, cnt.v), 1)]
 
-proc sum*(c: Multiset): Multiset =
+proc sum*(c: Collection): Collection =
   return c.reduce(sum_inner)
 
 proc distinct_inner(rows: seq[Row]): seq[Row] =
@@ -127,8 +130,8 @@ proc distinct_inner(rows: seq[Row]): seq[Row] =
     if i != 0:
       result.add((e, 1))
 
-## Reduce a multiset to a set
-proc `distinct`*(c: Multiset): Multiset =
+## Reduce a collection to a set
+proc `distinct`*(c: Collection): Collection =
   return c.reduce(distinct_inner)
 
 proc min_inner(rows: seq[Row]): seq[Row] =
@@ -152,7 +155,7 @@ proc min_inner(rows: seq[Row]): seq[Row] =
   else:
     return @[]
 
-proc min*(c: Multiset): Multiset =
+proc min*(c: Collection): Collection =
   try:
     return c.reduce(min_inner)
   except TypeException as e:
@@ -179,19 +182,23 @@ proc max_inner(rows: seq[Row]): seq[Row] =
   else:
     return @[]
 
-proc max*(c: Multiset): Multiset =
+proc max*(c: Collection): Collection =
   try:
     return c.reduce(max_inner)
   except TypeException as e:
     raise newException(TypeException, "Incomparable types")
 
-proc iterate*(c: Multiset, f: IterateFn): Multiset =
+proc iterate*(c: Collection, f: IterateFn): Collection =
   var curr = c
   while true:
     result = f(curr)
     if curr == result: break
     curr = result
 
-proc init_multiset*(rows: openArray[Row]): Multiset =
+proc init_collection*(rows: openArray[Row]): Collection =
   for r in rows:
     result.add(r)
+
+# Difference Collection #
+# ---------------------------------------------------------------------
+
--- tests/dida_from_python.nim
+++ tests/dida_from_python.nim
@@ -1,6 +1,6 @@
 import ../src/[test_utils, dida_from_python, values]
 
-template MSET*(rows: openArray[Row]): Multiset = init_multiset(rows)
+template COL*(rows: openArray[Row]): Collection = init_collection(rows)
 
 template STR*(): ImValue = init_string().v
 template STR*(s: string): ImValue = init_string(s).v
@@ -12,50 +12,50 @@ template MAP*(): ImValue = init_map().v
 template MAP*(vs: openArray[(ImValue, ImValue)]): ImValue = init_map(vs).v
 
 proc main* =
-  suite "multiset":
+  suite "collection":
     test "simple":
       var
-        a = MSET([])
-        b = MSET([])
-        c = MSET([((0.0.v, 1.0.v), 1)])
+        a = COL([])
+        b = COL([])
+        c = COL([((0.0.v, 1.0.v), 1)])
       check a == b
       check a != c
 
     test "various":
       var
-        a = MSET([
+        a = COL([
           ((STR"apple", STR"$5"), 2),
           ((STR"banana", STR"$2"), 1)
         ])
-        b = MSET([
+        b = COL([
           ((STR"apple", STR"$3"), 1),
           ((STR"apple", ARR([STR"granny smith", STR"$2"])), 1),
           ((STR"kiwi", STR"$2"), 1)
         ])
-        c = MSET([
+        c = COL([
           ((STR"apple", STR"$5"), 2),
           ((STR"banana", STR"$2"), 1),
           ((STR"apple", STR"$2"), 20),
         ])
-        d = MSET([
+        d = COL([
           ((STR"apple", 11.0.v), 1),
           ((STR"apple", 3.0.v), 2),
           ((STR"banana", 2.0.v), 3),
           ((STR"coconut", 3.0.v), 1),
         ])
         # some results
-        a_concat_b_result = MSET([
+        a_concat_b_result = COL([
           ((STR"apple", STR"$5"), 2),
           ((STR"banana", STR"$2"), 1),
           ((STR"apple", STR"$3"), 1),
           ((STR"apple", ARR([STR"granny smith", STR"$2"])), 1),
           ((STR"kiwi", STR"$2"), 1)
         ])
-        a_join_b_result = MSET([
+        a_join_b_result = COL([
           ((STR"apple", ARR([STR"$5", STR"$3"])), 2),
           ((STR"apple", ARR([STR"$5", ARR([STR"granny smith", STR"$2"])])), 2),
         ])
-        b_join_a_result = MSET([
+        b_join_a_result = COL([
           ((STR"apple", ARR([STR"$3", STR"$5"])), 2),
           ((STR"apple", ARR([ARR([STR"granny smith", STR"$2"]), STR"$5"])), 2),
         ])
@@ -63,63 +63,63 @@ proc main* =
       check b.concat(a) == a_concat_b_result
       check a.join(b) == a_join_b_result
       check b.join(a) == b_join_a_result
-      check a.filter(proc (e: Entry): bool = e.key == STR"apple") == MSET([
+      check a.filter(proc (e: Entry): bool = e.key == STR"apple") == COL([
         ((STR"apple", STR"$5"), 2)
       ])
-      check a.map(proc (e: Entry): Entry = (e.value, e.key)) == MSET([
+      check a.map(proc (e: Entry): Entry = (e.value, e.key)) == COL([
         ((STR"$5", STR"apple"), 2),
         ((STR"$2", STR"banana"), 1)
       ])
-      check a.concat(b).count() == MSET([
+      check a.concat(b).count() == COL([
         ((STR"apple", 4.0.v), 1),
         ((STR"banana", 1.0.v), 1),
         ((STR"kiwi", 1.0.v), 1)
       ])
-      check a.concat(b).distinct() == MSET([
+      check a.concat(b).distinct() == COL([
         ((STR"apple", STR"$5"), 1),
         ((STR"banana", STR"$2"), 1),
         ((STR"apple", STR"$3"), 1),
         ((STR"apple", ARR([STR"granny smith", STR"$2"])), 1),
         ((STR"kiwi", STR"$2"), 1)
       ])
-      check d.min() == MSET([
+      check d.min() == COL([
         ((STR"apple", 3.0.v), 1),
         ((STR"banana", 2.0.v), 1),
         ((STR"coconut", 3.0.v), 1),
       ])
-      check d.max() == MSET([
+      check d.max() == COL([
         ((STR"apple", 11.0.v), 1),
         ((STR"banana", 2.0.v), 1),
         ((STR"coconut", 3.0.v), 1),
       ])
-      check d.sum() == MSET([
+      check d.sum() == COL([
         ((STR"apple", 17.0.v), 1),
         ((STR"banana", 6.0.v), 1),
         ((STR"coconut", 3.0.v), 1),
       ])
-      check c.min() == MSET([
+      check c.min() == COL([
         ((STR"apple", STR"$2"), 1),
         ((STR"banana", STR"$2"), 1),
       ])
-      check c.max() == MSET([
+      check c.max() == COL([
         ((STR"apple", STR"$5"), 1),
         ((STR"banana", STR"$2"), 1),
       ])
     
     test "negate":
-      var a = MSET([
+      var a = COL([
         ((STR"foo", Nil.v), 3),
         ((STR"foo", Nil.v), 1),
         ((STR"bar", Nil.v), 2),
       ])
-      check a.negate == MSET([
+      check a.negate == COL([
         ((STR"foo", Nil.v), -3),
         ((STR"foo", Nil.v), -1),
         ((STR"bar", Nil.v), -2),
       ])
 
     test "consolidate":
-      var a = MSET([
+      var a = COL([
         ((STR"foo", Nil.v), 1),
         ((STR"foo", Nil.v), 3),
         ((STR"bar", Nil.v), 3),
@@ -130,21 +130,21 @@ proc main* =
         ((STR"bar", Nil.v), -47),
         ((STR"was", Nil.v), -3),
       ])
-      check a.consolidate == MSET([
+      check a.consolidate == COL([
         ((STR"foo", Nil.v), 14),
         ((STR"bar", Nil.v), -41),
       ])
-      check a.concat(a.negate).consolidate == MSET([])
+      check a.concat(a.negate).consolidate == COL([])
 
     test "iterate":
-      var a = MSET([((1.0.v, Nil.v), 1)])
-      proc add_one(c: Multiset): Multiset =
+      var a = COL([((1.0.v, Nil.v), 1)])
+      proc add_one(c: Collection): Collection =
         return c.map(proc (e: Entry): Entry = ((e.key.as_f64 + 1.0).v, e.value))
           .concat(c)
           .filter(proc (e: Entry): bool = e.key < 5.0.v)
           .distinct
           .consolidate
-      check a.iterate(add_one) == MSET([
+      check a.iterate(add_one) == COL([
         ((1.0.v, Nil.v), 1),
         ((2.0.v, Nil.v), 1),
         ((3.0.v, Nil.v), 1),
